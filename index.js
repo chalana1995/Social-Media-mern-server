f5ae93893ea57a5ca96234efd0812925b95e5ad9
chalanaK95 <ChalanaK@fortude.co>
2022-06-08 20:19:47 +0530

    file structure created

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from mongoose;
-import cors from cors
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRouter from './routes/posts.js'
+
+const app = express();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+app.use('/posts',postRouter)
+
+const CONNECTION_URL = "mongodb+srv://chalana:chalana123@cluster0.fb9v96d.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running port on ${PORT}`)))
+    .catch((err) => console.log("Error", err));
+
+// mongoose.set('useFindAndModify', false);
