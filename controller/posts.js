639f06b935b6907d825e5af086e8453124a6184c
chalanaK95 <ChalanaK@fortude.co>
2022-06-21 21:32:30 +0530

    add post

--- controller/posts.js
+++ controller/posts.js
@@ -1,3 +1,25 @@
-export const getPosts = (req,res) => {
-    res.send('Hello post')
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find()
+
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const createPost = async (req, res) => {
+    let post = rea.body;
+
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save();
+
+        res.status(200).json(newPost)
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
