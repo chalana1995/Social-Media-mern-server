6527e75ad7109620a8f08acf43607c77a6067237
chalanaK95 <ChalanaK@fortude.co>
2022-07-15 00:09:30 +0530

    modify code

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRouter from './routes/posts.js'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
@@ -14,10 +16,9 @@ app.use(cors())
 
 app.use('/posts',postRouter)
 
-const CONNECTION_URL = "mongodb+srv://chalana:chalana123@cluster0.fb9v96d.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running port on ${PORT}`)))
     .catch((err) => console.log("Error", err));
 
